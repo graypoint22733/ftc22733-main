69e52b2d05b6269051803f5ffbdb7c48e59b662c
graypoint22733 <robertmessier315@gmail.com>
2025-12-10 16:56:57 -0500

    Initialize module targets from encoder readings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SwerveTeleOpConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SwerveTeleOpConfig.java
@@ -8,6 +8,7 @@ public class SwerveTeleOpConfig {
     public static double Kp = 0.1, Kd = 0.002, Ki = 2, Kf = 0.5, Kl = 0.5;
     public static double module1Adjust = 337, module2Adjust = 285, module3Adjust = 0;
     public static double driveScale = 1.0, rotationScale = 1.0;
+    public static boolean useImu = false, fieldCentric = false;
 
     // Turret tuning
     public static double yawScale = 0.5;
